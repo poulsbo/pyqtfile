ec3a824d9fc269998ca8d113fac47b55d13fd4d6
Alan Liu <alansliu@gmail.com>
2020-12-12 21:46:42 -0800

    Add rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyqtfile',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Alan Liu',
+    author_email='alansliu@gmail.com',
+    description=''
+)
